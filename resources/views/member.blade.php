d80b4a806bc97026453d571d2c0b1324a27073a0
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2023-09-24 18:36:12 +0900

    update: メンバー　スタイル調整とsp版のメンバー部分の修正完了
    スタイル調整とsp版のメンバー部分の修正完了

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -7,37 +7,105 @@
 <div class="member-introduction-wrapper">
     {{-- タイトル --}}
     <div class="member-introduction-title">
-        <h1 class="member-introduction-h1">
+        <h2 class="member-introduction-h1">
             INTRODUCTION
-        </h1>
+        </h2>
         <p class="member-introduction-subtitle">
-            募集要項・選考フロー
+            社員紹介
         </p>
     </div>
 
     {{-- タイトルアンカー --}}
-    <div class="requirements-anchor">
+    <div class="member-anchor">
         <ul>
-            <li><h6>メンバー</h6></li>
-            <li><h6>社員インタビュー</h6></li>
-            <li><h6>キャリアパス</h6></li>
+            <li>
+                <h3>メンバー</h3>
+            </li>
+            <li>
+                <h3>社員インタビュー</h3>
+            </li>
+            <li>
+                <h3>キャリアパス</h3>
+            </li>
         </ul>
     </div>
 
     {{-- MEMBER --}}
-    <div class="member-wrapper">
+    <section>
         <div class="b-heading">
-            <span>
+            <h2>
                 MEMBER
-            </span>
+            </h2>
             <h3>
                 メンバー
             </h3>
+            <p class="member-message">
+                PlaylistはIT未経験からエンジニアを目指している人が多い会社です。社内メンバーと共に一緒に成長していける環境を整えていますので、エンジニアになるためのキャリアを私たちと共に描きましょう！！
+            </p>
+            <ul>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_ryo.jpeg')}}" alt="">
+                    <p class="post">CEO</p>
+                    <h4>Yuta.Y</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_hoshino.jpeg')}}" alt="">
+                    <p>COO/SALES</p>
+                    <h4>Takahiro.A</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_ryo.jpeg')}}" alt="">
+                    <p>CHO/HR/PR</p>
+                    <h4>Taro.T</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_hoshino.jpeg')}}" alt="">
+                    <p>BDR</p>
+                    <h4>Minami.Y</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_ryo.jpeg')}}" alt="">
+                    <p>ENGINEER</p>
+                    <h4>Mao.O</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_hoshino.jpeg')}}" alt="">
+                    <p>ENGINEER</p>
+                    <h4>Takuya.M</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_ryo.jpeg')}}" alt="">
+                    <p>ENGINEER</p>
+                    <h4>Ryo.W</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_hoshino.jpeg')}}" alt="">
+                    <p>ENGINEER</p>
+                    <h4>Masahiro.S</h4>
+                </li>
+                <li>
+                    <!-- TODO：画像を修正 -->
+                    <img src="{{asset('img/member/career_ryo.jpeg')}}" alt="">
+                    <p>ENGINEER</p>
+                    <h4>Yui.O</h4>
+                </li>
+                <li class="andmore">
+                    <p>and more...</p>
+                </li>
+            </ul>
         </div>
-    </div>
+    </section>
 
     {{-- INTERBVIEW --}}
-    <div class="interview-wrapper">
+    <section">
         <div class="b-heading">
             <span>
                 INTERBVIEW
@@ -46,23 +114,23 @@
                 社員インタビュー
             </h3>
         </div>
-    </div>
+        </section>
 
-    {{-- CAREER PATH --}}
-    <div class="career-path-wrapper">
-        <div class="b-heading">
-            <span>
-                CAREER PATH
-            </span>
-            <h3>
-                入社後のキャリアパス
-            </h3>
+        {{-- CAREER PATH --}}
+        <div class="career-path-wrapper">
+            <div class="b-heading">
+                <span>
+                    CAREER PATH
+                </span>
+                <h3>
+                    入社後のキャリアパス
+                </h3>
+            </div>
+            <div class="b-heading-about">
+                <p>Playlist社員の入社後のキャリアパスをご紹介します。</p>
+                <span class="annotation">※情報は2023年8月時点のものです。</span>
+            </div>
         </div>
-        <div class="b-heading-about">
-            <p>Playlist社員の入社後のキャリアパスをご紹介します。</p>
-            <span class="annotation">※情報は2023年8月時点のものです。</span>
-        </div>
-    </div>
 </div>
 
 
