611707324d6f9183b0e2afdcee6d6fece28e5d44
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2023-09-26 23:54:09 +0900

    update: 依存関係の修復
    エントリーボタンとヘッダーとフッターの依存関係がおかしくなっていたので修正

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Playlist採用サイト</title>
+  @vite(['resources/scss/style.scss', 'resources/js/app.js'])
+
+</head>
+
+<body>
+  @yield('header')
+
+  @yield('content')
+
+  @yield('footer')
+</body>
+
+</html>
