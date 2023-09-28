234dfa7d2109e8571606edc4e61640654d0ec8d9
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2023-09-28 11:32:09 +0900

    update: INTERVIEW部分sp版対応完了
    INTERVIEW部分のsp版htmlとcss対応完了

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -103,36 +103,90 @@
     </section>
 
     {{-- INTERBVIEW --}}
-    <section">
+    <section id="interview">
+        <h2>
+            INTERVIEW
+        </h2>
+        <h3>
+            社員インタビュー
+        </h3>
+        <ul>
+            <li class="interview-list">
+                <div class="interview-list_img">
+                    <img src="{{asset('img/member/interview_kigure.jpeg')}}" alt="木暮暁のインタビュー">
+                </div>
+                <p class="interview-list_date">
+                    <time datetime="2023-05-09">2023/05/09</time>
+                </p>
+                <p class="interview-list_content">
+                    【エンジニアインタビュー】正直モチベーションが下がったらどうしてる？
+                </p>
+            </li>
+            <li class="interview-list">
+                <div class="interview-list_img">
+                    <img src="{{asset('img/member/interview_hasegawa.jpeg')}}" alt="長谷川太一のインタビュー">
+                </div>
+                <p class="interview-list_date">
+                    <time datetime="2023-04-27">2023/04/27</time>
+                </p>
+                <p class="interview-list_content">
+                    【体験談】「自分の手でサービスを作れたら」淡い気持ちが形になり始めた…初プロジェクトを終えて長谷川さんへインタビュー
+                </p>
+            </li>
+            <li class="interview-list">
+                <div class="interview-list_img">
+                    <img src="{{asset('img/member/interview_yoshimura.jpeg')}}" alt="吉村優太のインタビュー">
+                </div>
+                <p class="interview-list_date">
+                    <time datetime="2023-04-20">2023/04/20</time>
+                </p>
+                <p class="interview-list_content">
+                    【CEOインタビュー】「最初はPCの電源の付け方もわからなかった」エンジニア歴15年以上、吉村が目指すPlaylistとは？
+                </p>
+            </li>
+            <li class="interview-list">
+                <div class="interview-list_img">
+                    <img src="{{asset('img/member/interview_hiruma.jpeg')}}" alt="昼間みなみのインタビュー">
+                </div>
+                <p class="interview-list_date">
+                    <time datetime="2023-04-13">2023/04/13</time>
+                </p>
+                <p class="interview-list_content">
+                    【Java研修を終えて】目標はアプリ開発！入社3カ月での変化
+                </p>
+            </li>
+        </ul>
+        <div class="interview-seemore">
+            <a href="https://www.wantedly.com/companies/Playlist0623/stories" target="_blank" class="interview-seemore_btn">
+                もっと見る
+                <svg class="interview-seemore_jump" xmlns="http://www.w3.org/2000/svg" width="16" height="15" viewBox="0 0 16 15" fill="none">
+                    <rect x="1.25" y="3.75" width="10.5" height="10.5" rx="1.25" stroke="#333333" stroke-width="1.5" />
+                    <rect x="4.25" y="0.75" width="10.5" height="10.5" rx="1.25" fill="white" stroke="#333333" stroke-width="1.5" />
+                </svg>
+            </a>
+            <p class="interview-seemore_kome">
+                ※外部サイトWantedlyへ遷移します
+            </p>
+        </div>
+    </section>
+
+    {{-- エントリーボタンの呼び出し --}}
+    @yield('entry_btn')
+
+    {{-- CAREER PATH --}}
+    <div class="career-path-wrapper">
         <div class="b-heading">
             <span>
-                INTERBVIEW
+                CAREER PATH
             </span>
             <h3>
-                社員インタビュー
+                入社後のキャリアパス
             </h3>
         </div>
-        </section>
-
-        {{-- エントリーボタンの呼び出し --}}
-        @yield('entry_btn')
-
-        {{-- CAREER PATH --}}
-        <div class="career-path-wrapper">
-            <div class="b-heading">
-                <span>
-                    CAREER PATH
-                </span>
-                <h3>
-                    入社後のキャリアパス
-                </h3>
-            </div>
-            <div class="b-heading-about">
-                <p>Playlist社員の入社後のキャリアパスをご紹介します。</p>
-                <span class="annotation">※情報は2023年8月時点のものです。</span>
-            </div>
+        <div class="b-heading-about">
+            <p>Playlist社員の入社後のキャリアパスをご紹介します。</p>
+            <span class="annotation">※情報は2023年8月時点のものです。</span>
         </div>
+    </div>
 </div>
 @endsection
-
-
