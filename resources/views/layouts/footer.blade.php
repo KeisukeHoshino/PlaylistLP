6294e7649a0bddb8d7b86bbcb460fb9c1d52c55e
mao-ueda <mao.ueda@playlist.co.jp>
2023-10-04 21:45:07 +0900

    update: footer 遷移先追記
    各遷移先を設定し忘れていたので追記しました

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,26 +7,26 @@
   </div>
   <div class="l-footer_inner">
     <div class="l-footer_lead">
-      <a href="#" class="l-footer_lead_item-playlist"><img src="{{asset('img/layout/logo_playlist_footer.svg')}}" alt="Playlist"><span>コーポレートサイト</span></a>
+      <a href="https://playlist.co.jp/" target="_blank" class="l-footer_lead_item-playlist"><img src="{{asset('img/layout/logo_playlist_footer.svg')}}" alt="Playlist"><span>コーポレートサイト</span></a>
       <div class="l-footer_lead_external">
-        <a href="#" class="l-footer_lead_item-mynavi"><img src="{{asset('img/layout/logo_mynavi.svg')}}" alt="マイナビ"></a>
-        <a href="#" class="l-footer_lead_item-wantedly"><img src="{{asset('img/layout/logo_wantedly.svg')}}" alt="Wantedly"></a>
+        <a href="https://tenshoku.mynavi.jp/company/327586/" target="_blank" class="l-footer_lead_item-mynavi"><img src="{{asset('img/layout/logo_mynavi.svg')}}" alt="マイナビ"></a>
+        <a href="https://www.wantedly.com/companies/Playlist0623" target="_blank" class="l-footer_lead_item-wantedly"><img src="{{asset('img/layout/logo_wantedly.svg')}}" alt="Wantedly"></a>
       </div>
     </div>
     <div class="l-footer_sns">
       <p class="l-footer_sns_heading">各種SNS更新中！</p>
       <ul class="l-footer_sns_list">
         <li>
-          <a href="#"><img src="{{asset('img/layout/logo_x.svg')}}" alt="X（旧Twitter）"><span>会社公式</span></a>
+          <a href="https://twitter.com/Playlist0623" target="_blank"><img src="{{asset('img/layout/logo_x.svg')}}" alt="X（旧Twitter）"><span>会社公式</span></a>
         </li>
         <li>
-          <a href="#"><img src="{{asset('img/layout/logo_x.svg')}}" alt="X（旧Twitter）"><span>採用担当</span></a>
+          <a href="https://twitter.com/PlaylistRecruit" target="_blank"><img src="{{asset('img/layout/logo_x.svg')}}" alt="X（旧Twitter）"><span>採用担当</span></a>
         </li>
         <li>
-          <a href="#"><img src="{{asset('img/layout/logo_instagram.svg')}}" alt="Instagram"></a>
+          <a href="https://www.instagram.com/playlist.co.ltd/" target="_blank"><img src="{{asset('img/layout/logo_instagram.svg')}}" alt="Instagram"></a>
         </li>
         <li>
-          <a href="#"><img src="{{asset('img/layout/logo_facebook.svg')}}" alt="Facebook"></a>
+          <a href="https://www.facebook.com/Playlist0623" target="_blank"><img src="{{asset('img/layout/logo_facebook.svg')}}" alt="Facebook"></a>
         </li>
       </ul>
     </div>
