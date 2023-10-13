60ce02cb23ef6e42984aeadf15aff38380a78b6c
shuji <e175712@ie.u-ryukyu.ac.jp>
2023-10-13 15:57:28 +0900

    update: エントリー　PDFファイル、最大サイズ設定

--- resources/js/entry.js
+++ resources/js/entry.js
@@ -1,14 +1,28 @@
-// フォーム入力内容チェック
+// 読み込み時
+window.onload = function(){
+    formValueCheck();
+}
+// フォーム内容変更時
 document.querySelector(".entryform").addEventListener('change', () => {
+    formValueCheck();
+});
+// フォーム入力内容チェック関数
+function formValueCheck(){
     let name = document.forms[0].name.value;
     let furigana = document.forms[0].furigana.value;
     let email = document.forms[0].email.value;
     let tel = document.forms[0].tel.value;
     let pdf = document.forms[0].pdf.value;
-
+    let pdf_file = document.forms[0].pdf.files[0];
     // pdfが選択されている場合、input下のテキストをファイルパスに変更する
     if (pdf) {
-        document.querySelector("p.entryform-list_file_selectfile").textContent = pdf;
+        // ファイルサイズは最大1.6MB
+        if (pdf_file.size <= 1600000) {
+            document.querySelector("p.entryform-list_file_selectfile").textContent = pdf;
+        }else{
+            document.querySelector("p.entryform-list_file_selectfile").textContent = 'ファイルサイズが大きすぎます(1.6MBまで)';
+            pdf = '';
+        }
     }else{
         document.querySelector("p.entryform-list_file_selectfile").textContent = '選択されていません';
     }
@@ -19,4 +33,4 @@ document.querySelector(".entryform").addEventListener('change', () => {
     } else {
         document.querySelector("#entryform-check-btn > button").disabled = true;
     }
-})
+}
