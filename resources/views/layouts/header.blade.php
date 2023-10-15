8a69f0d91a41830bef9d25928864c4a186a1e759
KomatsuHikaru <hikaru.komatsu@playlist.co.jp>
2023-10-15 15:48:33 +0900

    update:header SP版アコーディオンの修正
    メニューが見えていない時のイベント無効化、メニュー内ENTRYボタン位置の修正、文字色＆リンク先の追記

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,35 +13,35 @@
             <div class="c-headerMenubar-nav">
                 <ul>
                     <li class="c-headerMenubar-nav-listCenter active" id="c-headerMenubar-navTop">
-                        <a href="#" id="c-headerMenubar-navTop__heading">TOP</a>
+                        <a href="/" id="c-headerMenubar-navTop__heading">TOP</a>
                         <div class="subMenu--display c-headerMenubar-navTop__subMenu">
                             <ul>
-                                <li><a href="#">playlistについて</a></li>
-                                <li><a href="#">代表メッセージ</a></li>
-                                <li><a href="#">実際に働く社員のplaylistお気に入りポイント</a></li>
-                                <li><a href="#">よくある質問</a></li>
+                                <li><a href="/#aboutus">playlistについて</a></li>
+                                <li><a href="/#message">代表メッセージ</a></li>
+                                <li><a href="/#playlist-points">実際に働く社員のplaylistお気に入りポイント</a></li>
+                                <li><a href="/#faq">よくある質問</a></li>
                             </ul>
                         </div>
                     </li>
                     <li class="c-headerMenubar-nav-listCenter" id="c-headerMenubar-navMember">
-                        <a href="#" id="c-headerMenubar-navMember__heading">社員紹介</a>
+                        <a href="member" id="c-headerMenubar-navMember__heading">社員紹介</a>
                         <div class="subMenu--display c-headerMenubar-navMember__subMenu">
                             <ul>
-                                <li><a href="#">メンバー</a></li>
-                                <li><a href="#">社員インタビュー</a></li>
-                                <li><a href="#">入社後のキャリアパス</a></li>
+                                <li><a href="member#member">メンバー</a></li>
+                                <li><a href="member#interview">社員インタビュー</a></li>
+                                <li><a href="member#career-path">入社後のキャリアパス</a></li>
                             </ul>
                         </div>
                     </li>
                     <li class="c-headerMenubar-nav-listCenter" id="c-headerMenubar-navRequirements">
-                        <a href="#" id="c-headerMenubar-navRequirements__heading">募集要項・選考フロー</a>
+                        <a href="requirements" id="c-headerMenubar-navRequirements__heading">募集要項・選考フロー</a>
                         <div class="subMenu--display c-headerMenubar-navRequirements__subMenu">
                             <ul>
-                                <li><a href="#">募集要項</a></li>
-                                <li><a href="#">歓迎スキル</a></li>
-                                <li><a href="#">将来キャリアアップ</a></li>
-                                <li><a href="#">プロジェクト例</a></li>
-                                <li><a href="#">選考フロー</a></li>
+                                <li><a href="requirements#requirements">募集要項</a></li>
+                                <li><a href="requirements#welcome-skils">歓迎スキル</a></li>
+                                <li><a href="requirements#career-advancement">将来キャリアアップ</a></li>
+                                <li><a href="requirements#project-case">プロジェクト例</a></li>
+                                <li><a href="requirements#selection-flow">選考フロー</a></li>
                             </ul>
                         </div>
                     </li>
