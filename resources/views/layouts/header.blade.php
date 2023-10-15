a01e7b81ea76a8e6dc3b3573cf5ef61533d40963
KomatsuHikaru <hikaru.komatsu@playlist.co.jp>
2023-10-15 12:31:54 +0900

    update:header ENTRYボタンのホバー挙動修正
    ENTRYボタンのカーソルをポインターへ修正、TOPのKVにあるENTRYボタンとホバー挙動を合わせる

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -48,14 +48,7 @@
                 </ul>
             </div>
             <div class="c-header-entryBtn">
-                <a href="/entry" id="id_c-header-entryBtn">
-                    <span class="entry-btn_grad">
-                        ENTRY
-                        <svg class="entry-btn_arrow" xmlns="http://www.w3.org/2000/svg" width="10" height="11" viewBox="0 0 15 15" fill="none">
-                            <path d="M8.2458 14.394C7.93621 14.7087 7.36656 14.4254 7.36991 13.9583L7.46358 1.04965C7.46674 0.611324 7.98084 0.43609 8.30287 0.76349L14.5124 7.07647C14.7823 7.35096 14.7817 7.79618 14.5108 8.06954L8.2458 14.394Z" fill="#333333" />
-                        </svg>
-                    </span>
-                </a>
+                <a href="/entry" class="entry-btn_grad">ENTRY</a>
             </div>
         </nav>
         <div class="toggle-btn">
