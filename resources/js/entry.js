cd1df57069424bca759d68a1ffcb9925cc94ada8
shuji <e175712@ie.u-ryukyu.ac.jp>
2023-10-18 21:02:23 +0900

    update: エントリー　メールバリデーション(errorMassageCheck)削除

--- resources/js/entry.js
+++ resources/js/entry.js
@@ -1,24 +1,13 @@
 // 読み込み時
 window.onload = function(){
     formValueCheck();
-    errorMassageCheck();
 }
 // フォーム内容変更時
 // dataは変更された対象データ
 document.querySelector(".entryform").addEventListener('change', (data) => {
     formValueCheck(data);
-    errorMassageCheck();
 });
 
-// エラーメッセージが存在する場合エラーメッセージのコンテナ表示
-function errorMassageCheck(){
-    let ul_element = document.querySelector("div.error-message-area > ul")
-    if (ul_element.firstElementChild == null) {
-        ul_element.parentElement.style.display = "none";
-    }else{
-        ul_element.parentElement.style.display = "block";
-    }
-}
 // フォーム入力内容チェック関数
 function formValueCheck(data){
     const name = document.forms[0].name.value;
@@ -27,15 +16,6 @@ function formValueCheck(data){
     const tel = document.forms[0].tel.value;
     const pdf = document.forms[0].pdf.value;
     const pdf_file = document.forms[0].pdf.files[0];
-    // メールアドレスのバリデーション
-    if (data != undefined && data.target.name == 'email') {
-        if (email != '' && !email.match(/^[a-z]+[a-z0-9_-]+@[a-z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i)) {
-            let new_error_element = document.createElement('li');
-            new_error_element.textContent = '・メールアドレスの形式が間違っています。';
-            let ul_element = document.querySelector("div.error-message-area > ul")
-            ul_element.insertBefore(new_error_element,ul_element.firstChild);
-        }
-    }
     // pdfが選択されている場合、input下のテキストをファイルパスに変更する
     if (pdf) {
         // ファイルサイズは最大1.6MB
