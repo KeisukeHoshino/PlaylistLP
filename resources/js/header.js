ea1471069e4ba61e60ebf340d535a6f174555a3d
ryowatanabe14 <ryo.watanabe@playlist.co.jp>
2023-10-19 21:29:14 +0900

    update: SP版ヘッダーのaccordion修正対応

--- resources/js/header.js
+++ resources/js/header.js
@@ -4,7 +4,26 @@
 
 const nav = document.querySelector("#navArea");
 const btn = document.querySelector(".toggle-btn");
+const accordionNavItems = document.querySelectorAll(".c-headerMenubar-nav-listCenter");
+const accordionNavSubItems = document.querySelectorAll(".c-headerMenubar-nav-listCenter .subMenu--display");
 
 btn.onclick = () => {
     nav.classList.toggle("open");
 };
+
+accordionNavItems.forEach(accordionNavItem => {
+    accordionNavItem.onclick = () => {
+        if (accordionNavItem.classList.contains('is-open')) {
+            // クリックしたli要素からクラスを削除します
+            accordionNavItem.classList.remove('is-open');
+        } else {
+            // すべてのli要素からクラスを削除します
+            accordionNavItems.forEach((accordionNavItem) => {
+                accordionNavItem.classList.remove('is-open');
+            });
+
+            // クリックしたli要素にクラスを追加します
+            accordionNavItem.classList.add('is-open');
+        }
+    };
+});
