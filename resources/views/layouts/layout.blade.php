b9ab063da60aff06d613a10686bec4a9493c4bd8
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2023-12-13 19:28:42 +0900

    update: Google Analyticsの設定追加

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -26,6 +26,19 @@
 
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B9T941X1FD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-B9T941X1FD');
+</script>
+
 <body>
   @yield('header')
 
