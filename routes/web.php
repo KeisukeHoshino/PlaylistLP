8c6b95f538dba83dd61d8988c40c0ca8fa37fe97
mao-ueda <mao.ueda@playlist.co.jp>
2024-05-29 00:37:32 +0900

    update: メンバー モーダル内のテキスト修正

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/member', function () {
             "joinDate" => "2020年06月",
             "formerJob" => "フルスタックエンジニア",
             "myJob" => "経営、開発",
-            "skill" => "PHP/Javascript/HTML/CSS/VBA/C/C++",
+            "skill" => "PHP、Javascript、HTML、CSS、VBA、C、C++",
             "hobby" => "TVを見ることです。バラエティやドラマ関係なくテレビが大好きです。",
             "favoritePoint" => "様々なことに挑戦できる会社があったらいいなと思って創業しました。",
             "message" => "ITで挑戦したい事がある方、何事も最後までやり遂げる自信がある方大歓迎です。"
@@ -35,7 +35,7 @@ Route::get('/member', function () {
             "message" => "会社や事業を大きく成長できる体験が得られる会社です！将来きっと役にたつ経験なので、ぜひ立ち上げを経験したい方は待っています！"
         ],
         [
-            "position" => "CHO/HR/PR",
+            "position" => "CHRO",
             "name" => "Taro.T",
             "joinDate" => "2020年7月",
             "formerJob" => "営業職",
@@ -46,23 +46,23 @@ Route::get('/member', function () {
             "message" => "Playlistでの新しい発見や数々の挑戦が今後の自分の可能性を広げることだけでなく、素晴らしい成長に繋がります。私たちと共に、夢に向かって頑張りましょう！"
         ],
         [
-            "position" => "BDR",
+            "position" => "SALES/ENGINEER",
             "name" => "Minami.Y",
             "joinDate" => "2020年12月",
             "formerJob" => "商品事業のブランド管理部(業務は商品のパッケージデザイン)",
             "myJob" => "営業、RPA開発、その他社内業務",
-            "skill" => "VBA、SQL、Uipath",
+            "skill" => "VBA、SQL、UiPath",
             "hobby" => "旅行！温泉！美味しいごはんを食べること！映画！適度な美容！仕事も好きです！",
             "favoritePoint" => '社員全員の"人がいい"ところがお気に入りです！あと、社風や雰囲気も好きです！自分の声が反映されるスピード感が早いところもお気に入りです！',
             "message" => "一緒にPlaylistで頑張っていける人、募集中です！"
         ],
         [
-            "position" => "BDR",
+            "position" => "SALES",
             "name" => "Yuta.M",
             "joinDate" => "2020年10月",
             "formerJob" => "技術職",
             "myJob" => "営業対応",
-            "skill" => "HTML、CSS、JavaScript、VBA、WinActor等",
+            "skill" => "HTML、CSS、JavaScript、VBA、WinActor",
             "hobby" => "野球、アニメ",
             "favoritePoint" => "キャリアアップや方向性について社内の方とも一緒に考えていただける環境がある。",
             "message" => "未経験から入社し、現場の経験や研修を得て少しずつスキルアップできたと実感しています！一度、Playlistでキャリアアップを目指してみませんか！"
@@ -76,7 +76,7 @@ Route::get('/member', function () {
             "skill" => "Java、HTML、CSS、JavaScript、VBA",
             "hobby" => "オンラインゲーム",
             "favoritePoint" => "会社全体で一人一人の進路を応援してくれる",
-            "message" => "フレンドリーな仲間たち一緒に成長できる会社です！一緒に成長してきませんか！"
+            "message" => "フレンドリーな仲間たちと一緒に成長できる会社です！一緒に成長してきませんか！"
         ],
         [
             "position" => "ENGINEER",
@@ -84,7 +84,7 @@ Route::get('/member', function () {
             "joinDate" => "2021年3月",
             "formerJob" => "エンジニア",
             "myJob" => "Salesforce開発支援",
-            "skill" => "SQL、HTML、CSS、APEX、VMware、AWS、teraterm、Jmeter",
+            "skill" => "SQL、HTML、CSS、Apex、VMware、AWS、Tera Term、JMeter",
             "hobby" => "運動、お酒を飲むこと",
             "favoritePoint" => "自身がやりたいことについて、背中を押してくれる",
             "message" => "常に好奇心を持ち、積極的に学び続けることで大きく成長できます！一緒に頑張りましょう！"
@@ -104,7 +104,7 @@ Route::get('/member', function () {
             "position" => "ENGINEER",
             "name" => "Ryo.W",
             "joinDate" => "2022年2月",
-            "formerJob" => "技術職(android端末のSIMカード機能実装、評価)",
+            "formerJob" => "技術職(Android端末のSIMカード機能実装、評価)",
             "myJob" => "社内システム開発、運用保守",
             "skill" => "HTML、CSS、JavaScript、VBA、C、Python、PHP",
             "hobby" => "体を動かすこと、映画鑑賞、読書",
@@ -117,7 +117,7 @@ Route::get('/member', function () {
             "joinDate" => "2022年6月",
             "formerJob" => "営業職(農芸温室)",
             "myJob" => "某外資企業のPower BI開発支援、保守",
-            "skill" => "言語：Java、VBA、GAS、HTML、CSS、ツール：salesforce、Power BI",
+            "skill" => "Java、VBA、GAS、HTML、CSS、Salesforce、Power BI",
             "hobby" => "最近クロスバイクを購入したのでサイクリングにハマっています！",
             "favoritePoint" => "Playlistの展望を分け隔てなくお話を伺えること、常日頃からロードマップや技術の相談をさせて頂ける環境",
             "message" => "Playlistは成長フェーズであるため、経験年数に関係なく挑戦できることが多く存在します。課題解決を通じて技術やマネジメント力などを一緒に高められたらいいなと思います！"
@@ -128,7 +128,7 @@ Route::get('/member', function () {
             "joinDate" => "2022年10月",
             "formerJob" => "倉庫業",
             "myJob" => "運用",
-            "skill" => "なし",
+            "skill" => "ネットワークの基礎知識、VBA、WinActor",
             "hobby" => "バイク",
             "favoritePoint" => "経営陣や社員同士の距離感が近いから意見を言いやすい",
             "message" => "具体的なことは入社してから決めればいいと思うので、気にせず応募してください。"
@@ -139,7 +139,7 @@ Route::get('/member', function () {
             "joinDate" => "2023年1月",
             "formerJob" => "接客業",
             "myJob" => "後期高齢者医療保険制度に関する業務支援のシステム開発",
-            "skill" => "Java、Cobol、SQL",
+            "skill" => "Java、COBOL、SQL",
             "hobby" => "旅行、アウトドアアクティビティ、お酒",
             "favoritePoint" => "社員一人一人にフォーカスを置き、各人が目指す理想のエンジニア像に向けて支援してくれる",
             "message" => "未経験の方へ、必要なのはスキルや知識でなく、やる気があればそれだけ大丈夫です！Playlistでエンジニアになって一緒に働きましょう。"
@@ -150,7 +150,7 @@ Route::get('/member', function () {
             "joinDate" => "2023年3月",
             "formerJob" => "接客業",
             "myJob" => "レジシステムのテスター",
-            "skill" => "SQL、python、VBA",
+            "skill" => "SQL、Python、VBA",
             "hobby" => "音楽を聴くこと、美術館巡り",
             "favoritePoint" => "便利なツールを社内で積極的に取り入れていること、社員のチャレンジしたい気持ちを応援してくれるところ",
             "message" => "未経験で入社しましたが、研修や現場経験を通して少しずつエンジニアとして成長できていることを実感しています！PlaylistでITに挑戦してみませんか！"
@@ -194,8 +194,8 @@ Route::get('/member', function () {
             "joinDate" => "2023年10月",
             "formerJob" => "学校家具の営業職",
             "myJob" => "無線基地局インテグレーション作業",
-            "skill" => "現場独自のツール、Excel等",
-            "hobby" => "話を聞くこと、すること",
+            "skill" => "現場独自のツール、Excel",
+            "hobby" => "話を聞くこと、話すこと",
             "favoritePoint" => "自社や現場で様々なバックグラウンドを持つ方々とお話しできること",
             "message" => "ご縁がありましたら色々お話しましょう！"
         ],
@@ -216,7 +216,7 @@ Route::get('/member', function () {
             "joinDate" => "2024年4月",
             "formerJob" => "看護師",
             "myJob" => "某通信会社の商用保守",
-            "skill" => "Ruby,HTML,CSS,JavaScript",
+            "skill" => "Ruby、HTML、CSS、JavaScript",
             "hobby" => "作曲と絵を描くことが趣味です！",
             "favoritePoint" => "1つの会社で開発を行うのではなく、SESで様々な会社の開発に携わり色々なスキルを伸ばせるところ。業界未経験の方の入社してからの感想やフォロー体制などが明示されているところ。",
             "message" => "未経験でSESを目指す方はぜひPlaylistへ！"
